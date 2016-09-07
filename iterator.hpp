5aa3e95ee24184f7885b3733c223060c1fe6ff96
sarrow104 <sarrow104@gmail.com>
2016-09-07 17:20:28 +0800

    coding style: clang-format

--- iterator.hpp
+++ iterator.hpp
@@ -1,13 +1,13 @@
-#ifndef  __ITERATOR_HPP_1449323147__
-#define  __ITERATOR_HPP_1449323147__
+#ifndef __ITERATOR_HPP_1449323147__
+#define __ITERATOR_HPP_1449323147__
 
 #include <type_traits>
 #include <utility>
 
 namespace ss1x {
-template<typename T>
+template <typename T>
 using iterator_value_type =
     typename std::remove_reference<decltype(*std::declval<T>())>::type;
-}
+}  // namespace ss1x
 
-#endif  /* __ITERATOR_HPP_1449323147__ */
+#endif /* __ITERATOR_HPP_1449323147__ */
