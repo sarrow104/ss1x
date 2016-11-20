4ce5286cc69d1f1a8169845b47494280a119a60c
sarrow104 <sarrow104@gmail.com>
2016-11-20 20:35:15 +0800

    add split_port_auto()

--- asio/utility.hpp
+++ asio/utility.hpp
@@ -13,6 +13,24 @@ namespace url {
 // 协议字符串、域名、显式端口号、pathcommand
 std::tuple<std::string, std::string, int, std::string> split(
     const std::string& url);
+
+inline auto split_port_auto(const std::string& url) -> decltype(ss1x::util::url::split(url))
+{
+    auto url_info = ss1x::util::url::split(url);
+    if (std::get<2>(url_info) <= 0) {
+        if (std::get<0>(url_info) == "http") {
+            std::get<2>(url_info) = 80;
+        }
+        else if (std::get<0>(url_info) == "https"){
+            std::get<2>(url_info) = 443;
+        }
+    }
+    if (std::get<3>(url_info).empty()) {
+        std::get<3>(url_info) = "/";
+    }
+    return std::move(url_info);
+}
+
 std::string join(
     const std::tuple<std::string, std::string, int, std::string>& url);
 std::string join(const std::string& protocal, const std::string& domain,
