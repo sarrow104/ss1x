4a6c41ea448c52d1af20905caaeb41bdcdf5c2e4
sarrow104 <sarrow104@gmail.com>
2016-11-20 20:38:43 +0800

    split user-agent

---
+++ asio/user_agent.hpp
@@ -0,0 +1,9 @@
+#pragma once
+namespace ss1x {
+namespace http {
+const char * user_agent_firefox = "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6\r\n";
+
+#define USER_AGENT_DEFAULT  ss1x::http::user_agent_firefox
+
+} // namespace http
+} // namespace ss1x
