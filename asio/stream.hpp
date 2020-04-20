1e080efc12008ddfbbd0b87be45c9eca0e34cf95
sarrow104 <sarrow104@gmail.com>
2020-04-20 13:42:11 +0800

    add asio::stream{} base

---
+++ asio/stream.hpp
@@ -0,0 +1,72 @@
+// ss1x/asio/stream.hpp
+#pragma once
+
+#include <sss/string_view.hpp>
+#include <sss/colorlog.hpp>
+
+#include <cstdlib>
+#include <functional>
+
+#include <boost/asio/error.hpp>
+
+namespace ss1x {
+
+class stream
+{
+public:
+    typedef std::function<void(sss::string_view s)> on_avial_out_func_type;
+
+    stream() {}
+    virtual ~stream() {};
+
+    void set_on_avail_out(const on_avial_out_func_type& func)
+    {
+        m_on_avail_out = func;
+    }
+
+    void set_on_avail_out(on_avial_out_func_type&& func)
+    {
+        m_on_avail_out = std::move(func);
+    }
+
+    void on_avail_out(sss::string_view s)
+    {
+        if (m_on_avail_out)
+        {
+            m_on_avail_out(s);
+        }
+    }
+
+    virtual int inflate(const char * data, size_t size, int* p_ec = nullptr) = 0;
+    int inflate(sss::string_view sv, int * p_ec = nullptr)
+    {
+        return this->inflate(sv.data(), sv.size(), p_ec);
+    }
+
+protected:
+
+    int on_err(
+        int bytes_transferred,
+        const boost::system::error_code& err,
+        int * p_ec)
+    {
+        if (err.value())
+        {
+            COLOG_ERROR(err.message());
+        }
+        if (p_ec) {
+            *p_ec = err.value();
+        }
+
+        // 解压发生错误, 通知用户并放弃处理.
+        if (!p_ec && err.value()) {
+            throw err;
+        }
+        return bytes_transferred;
+    }
+
+protected:
+    on_avial_out_func_type m_on_avail_out;
+};
+
+} // namespace ss1x
